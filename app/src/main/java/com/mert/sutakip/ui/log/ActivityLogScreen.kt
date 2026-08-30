d3964d93d2b26dc6e79574dfcc75245bedfdc205
MMertYeter <79535077+MMertYeter@users.noreply.github.com>
2026-08-30 13:04:46 +0300

    Add height import to ActivityLogScreen.kt

--- app/src/main/java/com/mert/sutakip/ui/log/ActivityLogScreen.kt
+++ app/src/main/java/com/mert/sutakip/ui/log/ActivityLogScreen.kt
@@ -3,6 +3,7 @@ package com.mert.sutakip.ui.log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
